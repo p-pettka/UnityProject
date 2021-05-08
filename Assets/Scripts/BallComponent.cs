9be484c92ef20d34abbdd1c1fdde283b88cdf220
Przemysław Pettka <p.pettka@gmail.com>
2021-05-08 18:07:38 +0100

    Added FPS counter

--- Assets/Scripts/BallComponent.cs
+++ Assets/Scripts/BallComponent.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class BallComponent : MonoBehaviour
 {
+    private int fps;
     // Start is called before the first frame update
     void Start()
     {
-        
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        fps = (int)(1 / Time.deltaTime);
+        Debug.Log("FPS: " + fps);
     }
 }

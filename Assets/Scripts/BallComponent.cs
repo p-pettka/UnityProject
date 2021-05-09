fc271e21d024b3ca35d71c18c8558a1f7d44011a
Przemysław Pettka <p.pettka@gmail.com>
2021-05-09 19:32:01 +0100

    Added ball scaling

--- Assets/Scripts/BallComponent.cs
+++ Assets/Scripts/BallComponent.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class BallComponent : MonoBehaviour
 {
-    private int fps;
+    public float ScaleSpeed = 1.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-      
+ 
     }
 
     // Update is called once per frame
     void Update()
     {
-        fps = (int)(1 / Time.deltaTime);
-        Debug.Log("FPS: " + fps);
+
+        if (transform.localScale != new Vector3(3, 3, 3))
+        {
+            transform.localScale += Vector3.one * ScaleSpeed;
+        }
     }
 }

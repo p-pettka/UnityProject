33e0ed48b91b1c1299163f7b99a834044045822f
proboszcz777 <watroba.grzegorz@gmail.com>
2021-06-14 20:58:06 +0200

    Update BeachBallLevitate.cs
    Consultation test from 14.06.2021

--- Assets/Scripts/BeachBallLevitate.cs
+++ Assets/Scripts/BeachBallLevitate.cs
@@ -11,12 +11,15 @@ public class BeachBallLevitate : MonoBehaviour
     private Vector3 m_loopPosition;
     private Vector3 m_loopScale;
     private float m_curYPos = 0.0f;
+    private float m_oldYPos = 0.0f;
     private float m_curZRot = 0.0f;
     private float m_curScale;
     private float startTime;
     private float elapsedTime;
     private bool isMoving;
     private int timer;
+    private int directionChangeCount;
+
 
     public float MaxSize = 3.0f;
     public float Amplitude = 1.0f;
@@ -26,6 +29,40 @@ public class BeachBallLevitate : MonoBehaviour
     {
         while (true)
         {
+
+            if (isMoving)
+            {
+                m_oldYPos = m_curYPos;
+                m_curYPos = Mathf.PingPong(Time.time, Amplitude) - Amplitude * 0.5f;
+                transform.position = m_startPosition + Vector3.up * m_curYPos;
+
+                m_curZRot += Time.deltaTime * RotationSpeed;
+                transform.rotation = Quaternion.Euler(0, 0, m_curZRot);
+
+                m_curScale = (Mathf.PingPong(Time.time, Amplitude) - Amplitude * 0.5f) * MaxSize;
+                m_curScale = Mathf.Clamp(m_curScale, -m_scale.x, MaxSize);
+                transform.localScale = m_scale + Vector3.one * m_curScale;
+
+                if (m_oldYPos * m_curYPos < 0.0f)
+                    ++directionChangeCount;
+
+                if (directionChangeCount == 3 && Mathf.Abs(m_curYPos) < 0.01f)
+                {
+                    directionChangeCount = 1;
+                    isMoving = false;
+                }
+
+                yield return null;
+            }
+            else
+            {
+                yield return new WaitForSeconds(2);
+                isMoving = true;
+            }
+
+
+            
+            /*
             if (isMoving)
             {
                 m_curScale = Mathf.PingPong(elapsedTime, MaxSize - 1);
@@ -52,7 +89,7 @@ public class BeachBallLevitate : MonoBehaviour
             {
                 yield return new WaitForSeconds(2);
                 isMoving = true;
-            }
+            }*/
         }
     }
 
@@ -94,6 +131,9 @@ public class BeachBallLevitate : MonoBehaviour
         m_scale = transform.localScale;
         m_loopScale = transform.localScale;
         isMoving = true;
+        m_curYPos = 0.0f;
+        directionChangeCount = 0;
+        elapsedTime = 0.0f;
         //BeachBallAsync();
         StartCoroutine(BeachBallCoroutine());
     }
@@ -101,8 +141,9 @@ public class BeachBallLevitate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!isMoving) startTime = 0;
-        elapsedTime = Time.time - startTime;
+        elapsedTime += Time.deltaTime;
+        //if (!isMoving) startTime = 0;
+        //elapsedTime = Time.time - startTime;
         //transform.localScale = new Vector3(Mathf.PingPong(Time.time, MaxSize),transform.localScale.y, transform.localScale.z);
     }
 }

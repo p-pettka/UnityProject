d5da693047fddcbdd6d181e4c66fbd147a80a03e
Przemysław Pettka <p.pettka@gmail.com>
2021-07-31 14:44:14 +0100

    Added CreateAssetBundles

---
+++ Assets/Scripts/Editor/CreateAssetBundles.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class CreateAssetBundles
+{
+    [MenuItem("AssetBundles/Build All")]
+    private static void BuildAllAssetBundles()
+    {
+        string assetBundleDirectory = "Assets/StreamingAssets";
+
+        if (!Directory.Exists(Application.streamingAssetsPath))
+        {
+            Directory.CreateDirectory(assetBundleDirectory);
+            BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, EditorUserBuildSettings.activeBuildTarget);
+        }
+    }
+}

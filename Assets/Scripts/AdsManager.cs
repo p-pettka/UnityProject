72bfa8457edd8dae1dab86d078d013525b72e877
Przemysław Pettka <p.pettka@gmail.com>
2021-08-09 22:04:07 +0100

    Added banner Ads

---
+++ Assets/Scripts/AdsManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdsManager : Singleton<AdsManager>
+{
+    private const string ANDROID_AD_ID = "4255459";
+    private bool testMode = true;
+    private string bannerID = "banner";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Advertisement.Initialize(ANDROID_AD_ID, testMode);
+        StartCoroutine(ShowBannerWhenInitialized());
+    }
+
+    private IEnumerator ShowBannerWhenInitialized()
+    {
+        while (!Advertisement.isInitialized)
+        {
+            yield return new WaitForSeconds(0.5f);
+        }
+        Advertisement.Banner.SetPosition(BannerPosition.TOP_CENTER);
+        Advertisement.Show(bannerID);
+    }
+}

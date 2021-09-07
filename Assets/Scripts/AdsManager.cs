2a0ac631265a0ff1b03edde8e1d438b6b23d6131
Przemysław Pettka <p.pettka@gmail.com>
2021-09-07 23:25:40 +0200

    Added more particles to target prefab and updated AdsManager

--- Assets/Scripts/AdsManager.cs
+++ Assets/Scripts/AdsManager.cs
@@ -8,12 +8,13 @@ public class AdsManager : Singleton<AdsManager>
     private const string ANDROID_AD_ID = "4255459";
     private bool testMode = true;
     private string bannerID = "banner";
+    public int adsRemoved = 1;
 
     // Start is called before the first frame update
     void Start()
     {
-        PlayerPrefs.SetInt("AdsRemoved", 0);
-        int adsRemoved = PlayerPrefs.GetInt("AdsRemoved");
+        PlayerPrefs.SetInt("AdsRemoved", adsRemoved);
+        adsRemoved = PlayerPrefs.GetInt("AdsRemoved");
         Advertisement.Initialize(ANDROID_AD_ID, testMode);
         if (adsRemoved != 1) { StartCoroutine(ShowBannerWhenInitialized()); }
     }

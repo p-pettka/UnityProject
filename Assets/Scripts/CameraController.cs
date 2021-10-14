d790ea91500da9658ce07bcf78e1b5fea576a063
Przemysław Pettka <p.pettka@gmail.com>
2021-10-14 18:13:09 +0100

    Added drag camera movement

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,6 +4,8 @@ public class CameraController : MonoBehaviour
 {
     private BallComponent followTarget;
     private Vector3 originalPosition;
+    private float dragSpeed = 2;
+    private Vector3 dragOrigin;
 
     // Start is called before the first frame update
     void Start()
@@ -21,5 +23,19 @@ public class CameraController : MonoBehaviour
         {
             transform.position = originalPosition;
         }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            dragOrigin = Input.mousePosition;
+            return;
+        }
+
+        if (!Input.GetMouseButton(0)) return;
+
+        Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+        Vector3 move = new Vector3(pos.x * dragSpeed, 0, 0);
+
+        transform.Translate(move, Space.World);
+
     }
 }
